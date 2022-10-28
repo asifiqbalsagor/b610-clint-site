b5344dddb1f715a30f32c9ee88d1fe76b08dc626
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-10-28 16:56:17 +0600

    google sign in

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,27 +1,26 @@
-import React, { children } from 'react';
-import { createContext } from 'react';
-import app from '../../firebase-init/firebase.init';
-import { getAuth, signInWithPopup } from 'firebase/auth'
-import { useState } from 'react';
+import { createContext } from "react";
+import { getAuth, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+import app from "../firebase-config/firebase-config";
+export const AuthContext = createContext();
 
-const AuthContext = createContext();
+
 const auth = getAuth(app);
 const Context = ({ children }) => {
-     const [user, setUser] = useState({name:'sagor'});
-     const LoginWithGoogle = (provider) => {
-          return signInWithPopup(auth, provider);
-     }
-     
-     const userInfo = {
-          user,LoginWithGoogle
-     }
-     return (
-          <div>
-               <AuthContext.Provider
-                    value={userInfo}
-               >{ children}</AuthContext.Provider>
-          </div>
-     );
+  const [user, setUser] = useState({ name: "sagor" });
+  const LoginWithGoogle = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const userInfo = {
+    user,
+    LoginWithGoogle,
+  };
+  return (
+    <div>
+      <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
 };
 
 export default Context;
--- src/Login-Register/Login.js
+++ src/Login-Register/Login.js
@@ -1,25 +1,33 @@
+import { GoogleAuthProvider } from "firebase/auth";
 import React from "react";
 import { useContext } from "react";
 import { Form } from "react-router-dom";
-import {AuthContext} from "../Context/Context"
+import { AuthContext } from "../Context/Context.js";
 
 const Login = () => {
-     const { LoginWithGoogle } = useContext(AuthContext);
+  const { LoginWithGoogle } = useContext(AuthContext);
+  const googleProvider = new GoogleAuthProvider();
+  const handleLoginGoogle = () => {
+    LoginWithGoogle(googleProvider).then((result) => {
+      const user = result.user;
+      console.log(user);
+    });
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
-        <Form  className="">
+        <Form className="">
           <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-                           <div className="card-body">
-                                <button>Login with Google</button>
-                                <button>Login with Github</button>
+            <div className="card-body">
+              <button className="btn btn-outline">Login with Github</button>
+              <button onClick={handleLoginGoogle} className="btn btn-outline btn-primary">Login with Google</button>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                                          type="email"
-                                          name="email"
+                  type="email"
+                  name="email"
                   placeholder="email"
                   className="input input-bordered"
                 />
@@ -29,8 +37,8 @@ const Login = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input
-                                          type="password"
-                                          name="password"
+                  type="password"
+                  name="password"
                   placeholder="password"
                   className="input input-bordered"
                 />
