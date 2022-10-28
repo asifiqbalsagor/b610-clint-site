b60b2bac4f1126c928d0e9cf79eb45007ed58976
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-10-28 23:56:25 +0600

    anather cha

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Main from './Layout/Main/Main';
-import Courses from './Component/Courses/Courses';
-import FAQ from './Component/FAQ/Faq';
-import Blog from './Component/Blog/Blog';
-import Login from './Login-Register/Login';
-import Register from './Login-Register/Register';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Layout/Main/Main";
+import Courses from "./Component/Courses/Courses";
+import FAQ from "./Component/FAQ/Faq";
+import Blog from "./Component/Blog/Blog";
+import Login from "./Login-Register/Login";
+import Register from "./Login-Register/Register";
 
 function App() {
   const router = createBrowserRouter([
-    { 
+    {
       path: "/",
       element: <Main></Main>,
       children: [
         {
           path: "/courses",
           loader: () => {
-            return fetch('http://localhost:5000/allCourse')
+            return fetch(
+              "https://web-development-courses-server.vercel.app/allCourse"
+            );
           },
-          element:<Courses></Courses>
+          element: <Courses></Courses>,
         },
         {
           path: "/faq",
-          element:<FAQ></FAQ>
+          element: <FAQ></FAQ>,
         },
         {
           path: "/blog",
-          element:<Blog></Blog>
+          element: <Blog></Blog>,
         },
         {
           path: "/login",
-          element:<Login></Login>
+          element: <Login></Login>,
         },
         {
           path: "/register",
-          element:<Register></Register>
-        }
-      ]
-    }
+          element: <Register></Register>,
+        },
+      ],
+    },
   ]);
   return (
     <div className="App">
--- src/Component/SideBar/SideBar.js
+++ src/Component/SideBar/SideBar.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import ShowSideBar from '../ShowSideBar/ShowSideBar';
+import React, { useEffect, useState } from "react";
+import ShowSideBar from "../ShowSideBar/ShowSideBar";
 
 const SideBar = () => {
-     const [categories,setCategories] = useState([]);
-    useEffect( () => {
-        fetch('http://localhost:5000/course-categories')
-        .then(res => res.json())
-        .then(data => setCategories(data))
-    },[])
-     return (
-          <div>
-               {categories.map(category => <ShowSideBar
-                    key={category.id}
-                    category={category}
-               ></ShowSideBar>)}
-          </div>
-     );
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://web-development-courses-server.vercel.app/course-categories"
+    )
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div>
+      {categories.map((category) => (
+        <ShowSideBar key={category.id} category={category}></ShowSideBar>
+      ))}
+    </div>
+  );
 };
 
 export default SideBar;
